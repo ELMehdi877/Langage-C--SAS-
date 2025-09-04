05555144c385812e362f41a05c1aeda56f96706c
elmahdilahrache <lahrachelmehdi877@gmail.com>
2025-09-04 15:39:32 +0100

    fonction_table_de_multiplication_d'un_nbr

---
+++ fonction_table_de_multiplication_d'un_nbr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a=10;
+    if(1){
+       int a=0;
+       a=12;
+        printf("%d",a);
+    }
+    printf("%d",a);
+    
+return 0;
+}
